de5d221cb5ed92eaebb20e163addac68cd6561f8
Diego12323 <143857894+Diego12323@users.noreply.github.com>
2023-11-03 16:33:46 -0500

    Avance 3
    código en HTML

---
+++ 45-login/main.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const username = document.getElementById('username');
+    const password = document.getElementById('password');
+    const button = document.getElementById('button');
+
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        const data = {
+            username: username.value,
+            password: password.value
+        }
+
+        console.log(data);
+    });
+});
